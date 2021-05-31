d4bae999018cf486a78a1f1236c6e65853011bce
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2021-05-31 13:46:07 -0300

    eslint prettier editorconfig

---
+++ src/@types/declaration.d.ts
@@ -0,0 +1,12 @@
+// styles
+declare module '*.css';
+declare module '*.scss';
+declare module '*.sass';
+// images
+declare module '*.svg';
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.gif';
+//fonts
+declare module '*.ttf';
